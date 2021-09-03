331c5f098ee20af2febf09cdafcf07ca7fb900a3
ka1peshc <kalpesh111997@gmail.com>
2021-09-03 08:56:18 +0530

    Calculate winning percentage on heads vs tails

--- CorePracticeProblem/Program.cs
+++ CorePracticeProblem/Program.cs
@@ -4,9 +4,13 @@ namespace CorePracticeProblem
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            HeadsAndTails hat = new HeadsAndTails();
+            hat.HeadAndTail();
         }
     }
 }
