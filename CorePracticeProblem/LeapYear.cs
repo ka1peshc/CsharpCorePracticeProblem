e8493723bfb2e723848b9447a5fb4face0e7bdbc
ka1peshc <kalpesh111997@gmail.com>
2021-09-03 09:38:04 +0530

    Check if your user entered year is leap year or not

---
+++ CorePracticeProblem/LeapYear.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePracticeProblem
+{
+    class LeapYear
+    {
+        public Boolean checkLeapYear(int year)
+        {
+            if (year < 1000 || year > 9999)
+            {
+                Console.WriteLine("Enter year in valid range 1000 to 9999");
+                return false;
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    if (year % 100 == 0)
+                    {
+                        if (year % 400 == 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- CorePracticeProblem/Program.cs
+++ CorePracticeProblem/Program.cs
@@ -8,9 +8,23 @@ namespace CorePracticeProblem
         
         static void Main(string[] args)
         {
+            //Flip Coin and print percentage of Heads and Tails
             Console.WriteLine("Hello World!");
             HeadsAndTails hat = new HeadsAndTails();
             hat.HeadAndTail();
+            //Leap Year
+            Console.WriteLine("Enter the year to check if leap year or not");
+            int year=Convert.ToInt32(Console.ReadLine());
+            LeapYear ly = new LeapYear();
+            bool leap = ly.checkLeapYear(year);
+            if (leap)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year");
+            }
         }
     }
 }
